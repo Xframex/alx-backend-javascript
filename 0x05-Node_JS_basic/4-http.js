6ec48ed52a33667195e850790577471dd3bcf7a3
Xframex <loukili.issmael@.com>
2024-05-01 02:07:14 +0000

    http server fix

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -2,10 +2,19 @@ const http = require('http');
 
 const PORT = 1245;
 const HOST = 'localhost';
-const app = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('Hello Holberton School!');
-    res.end();
+const app = http.createServer();
+
+app.on('request', (_, res) => {
+  const responseText = 'Hello Holberton School!';
+
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', responseText.length);
+  res.statusCode = 200;
+  res.write(Buffer.from(responseText));
 });
-app.listen(PORT, HOST);
-console.log('Server running at http://localhost:1245/')
+
+app.listen(PORT, HOST, () => {
+  process.stdout.write(`Server listening at -> http://${HOST}:${PORT}\n`);
+});
+
+module.exports = app;
