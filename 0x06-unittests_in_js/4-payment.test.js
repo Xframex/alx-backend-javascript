63add72767c15ab74d5e4a5b64d16ac8e72911f1
Xframex <loukili.issmael@gmail.com>
2024-05-02 18:39:32 -0700

    test 5 fixstub

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,15 +1,20 @@
 const sinon = require('sinon');
 const Utils = require('./utils');
 const { expect } = require('chai');
-const sendPaymentRequestToApi = require('./3-payment');
+const sendPaymentRequestToApi = require('./4-payment');
 
 describe('sendPaymentRequestToApi', () => {
-  it('sendPaymentRequestToApi uses the calculateNumber method of Utils', () => {
-    const bigBrother = sinon.spy(Utils);
+  it('sendPaymentRequestToApi calls console.log with the right arguments', () => {
+    const startp = sinon.spy(console);
+    const endp = sinon.stub(Utils, 'calculateNumber');
 
+    endp.returns(10);
     sendPaymentRequestToApi(100, 20);
-    expect(bigBrother.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
-    expect(bigBrother.calculateNumber.callCount).to.be.equal(1);
-    bigBrother.calculateNumber.restore();
+    expect(endp.calledWith('SUM', 100, 20)).to.be.true;
+    expect(endp.callCount).to.be.equal(1);
+    expect(startp.log.calledWith('The total is: 10')).to.be.true;
+    expect(startp.log.callCount).to.be.equal(1);
+    endp.restore();
+    startp.log.restore();
   });
 });
